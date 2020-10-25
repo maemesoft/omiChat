71c9224f9d4fc3c93101c0b8b74adc6ffe88dc9e
maemesoft <maemesoft@naver.com>
2020-10-25 22:33:58 +0900

    Apply redux-persist to localForage for web

---
+++ src/modules/index.web.ts
@@ -0,0 +1,21 @@
+import localForage from "localforage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+import userInfoReducer from "./userInfo";
+import registerInfoReducer from "./registerInfo";
+
+// Declare userInfo's Persist Config to make reducer persist
+const userInfoPersistConfig = {
+    key: "userInfo",
+    storage: localForage,
+};
+
+const rootReducer = combineReducers({
+    userInfo: persistReducer(userInfoPersistConfig, userInfoReducer),
+    registerInfo: registerInfoReducer,
+});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
